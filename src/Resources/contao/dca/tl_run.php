c0b710ac7d2658b9bbbb727f9d473db4c45f92a9
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-01-22 15:11:03 +0100

    Create tl_run.php

---
+++ src/Resources/contao/dca/tl_run.php
@@ -0,0 +1,110 @@
+<?php
+/*
+** DCA für findme: tl_run
+** © 2020 Markus Schenker, Phi Network
+*/
+/*
+ * Table tl_course
+ */
+$GLOBALS['TL_DCA']['tl_run'] = array
+(
+	// Config
+	'config' => array
+	(
+		'dataContainer'               => 'Table',
+		'enableVersioning'            => true,
+		'sql' => array
+		(
+			'keys' => array
+			(
+				'id' => 'primary'
+			)
+		),
+	),
+	// List
+	'list' => array
+	(
+		'sorting' => array
+		(
+			'mode'                    => 1,
+			'fields'                  => array('title'),
+			'flag'                    => 8,
+			'panelLayout'             => 'filter;sort,search,limit'
+		),
+		'label' => array
+		(
+			'fields'                  => array('title'),
+			'format'                  => '%s',
+		),
+		'global_operations' => array
+		(
+			'all' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+				'href'                => 'act=select',
+				'class'               => 'header_edit_all',
+				'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+			)
+		),
+		'operations' => array
+		(
+			'edit' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_course']['edit'],
+				'href'                => 'act=edit',
+				'icon'                => 'edit.gif'
+			),
+			'delete' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_course']['delete'],
+				'href'                => 'act=delete',
+				'icon'                => 'delete.gif',
+				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+			),
+			'show' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_course']['show'],
+				'href'                => 'act=show',
+				'icon'                => 'show.gif',
+				'attributes'          => 'style="margin-right:3px"'
+			),
+		)
+	),
+	// Palettes
+	'palettes' => array
+	(
+		'default'	=> 'title;posten;creator'
+	),
+	// Fields
+	'fields' => array
+	(
+		'id' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+		),
+		'tstamp' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+		'courseid' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+    'runnerid' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+		'start' => array
+		(
+			'sql'                     => "varchar(10) NOT NULL default ''"
+		),
+    'ziel' => array
+		(
+			'sql'                     => "varchar(10) NOT NULL default ''"
+		),
+    'runtime' => array
+		(
+			'sql'                     => "varchar(10) NOT NULL default ''"
+		),
+	)
+);
