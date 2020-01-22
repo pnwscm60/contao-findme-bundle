e843d502c9178e08b310c853c934276ca1f40a8d
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-01-22 13:28:58 +0100

    Create Pnwscm60FindmeBundle.php

---
+++ src/Pnwscm60FindmeBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of FindmeBundle.
+ *
+ * (c) Markus Schenker
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Pnwscm60\FindmeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+/* Configure the Contao Findme Bundle */
+class Pnwscm60FindmeBundle extends Bundle
+{
+}
