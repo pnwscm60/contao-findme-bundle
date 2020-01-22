6754c4c40c3ceb6ddaaaffb6708da20da0f45871
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-01-22 14:49:12 +0100

    Update tl_course.php

--- src/Resources/contao/dca/tl_course.php
+++ src/Resources/contao/dca/tl_course.php
@@ -1,1 +1,141 @@
-tl_course.php
+<?php
+/*
+** DCA für findme: tl_course
+** © 2020 Markus Schenker, Phi Network
+*/
+/*
+ * Table tl_course
+ */
+$GLOBALS['TL_DCA']['tl_course'] = array
+(
+	// Config
+	'config' => array
+	(
+		'dataContainer'               => 'Table',
+		'enableVersioning'            => true,
+		'sql' => array
+		(
+			'keys' => array
+			(
+				'id' => 'primary'
+			)
+		),
+	),
+	// List
+	'list' => array
+	(
+		'sorting' => array
+		(
+			'mode'                    => 1,
+			'fields'                  => array('title'),
+			'flag'                    => 8,
+			'panelLayout'             => 'filter;sort,search,limit'
+		),
+		'label' => array
+		(
+			'fields'                  => array('title'),
+			'format'                  => '%s',
+		),
+		'global_operations' => array
+		(
+			'all' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+				'href'                => 'act=select',
+				'class'               => 'header_edit_all',
+				'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+			)
+		),
+		'operations' => array
+		(
+			'edit' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_course']['edit'],
+				'href'                => 'act=edit',
+				'icon'                => 'edit.gif'
+			),
+			'delete' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_course']['delete'],
+				'href'                => 'act=delete',
+				'icon'                => 'delete.gif',
+				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+			),
+			'show' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_course']['show'],
+				'href'                => 'act=show',
+				'icon'                => 'show.gif',
+				'attributes'          => 'style="margin-right:3px"'
+			),
+		)
+	),
+	// Palettes
+	'palettes' => array
+	(
+		'default'	=> 'title;posten;creator'
+	),
+	// Fields
+	'fields' => array
+	(
+		'id' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+		),
+		'tstamp' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+		'aktivvon' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_course']['aktivvon'],
+			'inputType'               => 'text',
+			'eval'                    => array('mandatory'=>false, 'unique'=>false,'tl_class'=>'w50'),
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+    'aktivbis' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_course']['aktivbis'],
+			'inputType'               => 'text',
+			'eval'                    => array('mandatory'=>false, 'unique'=>false,'tl_class'=>'w50'),
+			'sql'                     => "int(10) unsigned NOT NULL default '0'"
+		),
+		'title' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_course']['title'],
+			'inputType'               => 'text',
+			'exclude'                 => true,
+			'sorting'                 => true,
+			'flag'                    => 11,
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'unique'=>false, 'maxlength'=>255, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(128) NOT NULL default ''"
+		),
+    'ort' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_course']['ort'],
+			'inputType'               => 'text',
+			'exclude'                 => true,
+			'sorting'                 => true,
+			'flag'                    => 11,
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'unique'=>false, 'maxlength'=>255, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(128) NOT NULL default ''"
+		),
+    'posten' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_course']['posten'],
+			'inputType'               => 'text',
+			'exclude'                 => true,
+			'sorting'                 => true,
+			'flag'                    => 11,
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'unique'=>false, 'maxlength'=>255, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(128) NOT NULL default ''"
+		),
+    'creatorid' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+		),
+	)
+);
