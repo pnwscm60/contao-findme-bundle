7665bace76ac8cc20fefa51af76932d35b2ddc40
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2020-01-22 14:20:29 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,7 +3,7 @@
  * Back end modules
  * Front end modules
  */
-use Pnwscm60\ConcertoBundle\Module\ModuleCapture;
+use Pnwscm60\FindmeBundle\Module\ModuleCapture;
 
 $GLOBALS['FE_MOD']['findme'] = [ 
 	'capture' => ModuleCapture::class
